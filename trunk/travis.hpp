045eb75f8389bd6f682f5b18a6d12b5a8ffb7bcf
jgvictores <jgvictores@9f3ed7e0-da76-4f88-b5fc-cea1cb3ab327>
2012-09-15 10:43:35 +0000

    updated for OpenCV-2.4.2

--- trunk/travis.hpp
+++ trunk/travis.hpp
@@ -20,7 +20,8 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/core/core.hpp"
-#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/nonfree/features2d.hpp"  // SurfFeatureDetector. Before was: "opencv2/features2d/features2d.hpp"
+#include "opencv2/legacy/legacy.hpp"  // BruteForceMatcher. Before was: "opencv2/features2d/features2d.hpp"
 
 using namespace cv;
 
