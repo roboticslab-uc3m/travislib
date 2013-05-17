c091f6ce94ab38f53c7d1cdba931163a729c6ee1
jgvictores <jgvictores@9f3ed7e0-da76-4f88-b5fc-cea1cb3ab327>
2013-05-17 10:53:32 +0000

    naming

--- main/TravisLib.hpp
+++ main/TravisLib.hpp
@@ -1,18 +1,16 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
-
 /**
  * 
  * @ingroup travis_libraries
- * \defgroup SegExLib SegExLib
+ * \defgroup TravisLib TravisLib
  *
- * @brief The SegExLib library provides basic 2D image feature extraction auxiliary
+ * @brief The TravisLib library provides basic 2D image feature extraction auxiliary
  * functions for the \ref segEx module and others.
  * 
  * <hr>
  *
- * This file can be edited at $XGNITIVE_ROOT/main/src/libraries/SegExLib/SegexLib.hpp
+ * This file can be edited at $XGNITIVE_ROOT/main/src/libraries/TravisLib/SegexLib.hpp
  *
  */
 
@@ -25,55 +23,55 @@ using namespace std;
 using namespace cv;
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function gets the biggest contour.
  */
 vector <Point> getBiggestContour(const Mat image);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates X and Y.
  */
 void calcLocationXY(float& locX, float& locY, const vector <Point> biggestCont);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the mask.
  */
 void calcMask(Mat& mask, const vector <Point> biggestCont);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the area.
  */
 void calcArea(float& area, const vector <Point> biggestCont);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the rectangularity.
  */
 void calcRectangularity(float& rectangularity, const vector <Point> biggestCont);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the mass center.
  */
 void calcMassCenter(float& massCenterLocX, float& massCenterLocY , const vector <Point> biggestCont);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the aspect ratio.
  */
 void calcAspectRatio(float& aspectRatio, float& axisFirst, float& axisSecond ,const vector <Point> biggestCont);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the solidity.
  */
 void calcSolidity(float& solidity, const vector <Point> biggestCont);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the HSV mean and std deviation.
  */
 void calcHSVMeanStdDev(const Mat image, const Mat mask, float& hue_mean, float& hue_stddev,
@@ -81,26 +79,26 @@ void calcHSVMeanStdDev(const Mat image, const Mat mask, float& hue_mean, float& 
                        float& value_mean, float& value_stddev);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the HSV peak color.
  */
 void calcHSVPeakColor(const Mat image, const Mat mask, float& hue_mode, float& hue_peak,
                        float& value_mode, float& value_peak);
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the moments.
  */
 void calcMoments(Mat& theHuMoments, const vector <Point> biggestCont );
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the arc length.
  */
 void calcArcLength(float& arc, const vector <Point> biggestCont );
 
 /**
- * @ingroup SegExLib
+ * @ingroup TravisLib
  * This function calculates the circle.
  */
 void calcCircle(float& radius, const vector <Point> biggestCont );
