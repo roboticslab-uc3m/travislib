270041d8e2bdbc6715d350c49ba3bbc334c5d9b1
jgvictores <jgvictores@9f3ed7e0-da76-4f88-b5fc-cea1cb3ab327>
2013-05-18 22:16:05 +0000

    return 3 layered

--- main/TravisLib.cpp
+++ main/TravisLib.cpp
@@ -29,8 +29,10 @@ void Travis::binarize(const char* algorithm, const double threshold) {
         if (!_quiet) printf("[Travis] in: binarize: redMinusGreen.\n");
         cv::Mat bgrChannels[3];
         cv::split(_img, bgrChannels);
+        bgrChannels[1] = bgrChannels[0];
+        bgrChannels[2] = bgrChannels[0];
         //cv::subtract(
-        _img = bgrChannels[0];
+        cv::merge(bgrChannels, 3, _img);
     } else fprintf(stderr,"[warning] Unrecognized algorithm.\n");
     return;
 }
