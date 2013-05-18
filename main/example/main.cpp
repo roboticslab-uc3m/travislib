28a5061c84b75f39d1e6cc3b02f549be4a5b5775
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-05-18 17:03:05 +0000

    example prototype

---
+++ main/example/main.cpp
@@ -0,0 +1,34 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include <stdio.h>
+
+#include <cv.h>
+#include <cvaux.h>
+#include <highgui.h>
+
+int main(int argc, char *argv[]) {
+   
+    char* imageName = argv[1];
+
+    cv::Mat image;
+    image = cv::imread( imageName, 1 );
+
+    if( argc != 2 || !image.data ) {
+        printf( " No image data \n " );
+        return -1;
+    }
+
+    cv::Mat gray_image;
+    cvtColor( image, gray_image, CV_BGR2GRAY );
+
+    //imwrite( "../../images/Gray_Image.jpg", gray_image );
+
+    cv::namedWindow( imageName, CV_WINDOW_AUTOSIZE );
+    cv::namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
+
+    imshow( imageName, image );
+    imshow( "Gray image", gray_image );
+
+    cv::waitKey(0);
+    return 0;
+}
