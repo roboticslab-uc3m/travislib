b7f7839e9824febb1d39be286ce8cab2a2907f6f
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-05-23 22:30:48 +0000

    more work on doc

--- main/TravisLib.hpp
+++ main/TravisLib.hpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#ifndef __TRAVIS_LIB__
-#define __TRAVIS_LIB__
+#ifndef __TRAVIS_LIB_HPP__
+#define __TRAVIS_LIB_HPP__
 
 /**
  * 
@@ -39,7 +39,10 @@ using namespace cv;
  */
 class Travis {
 protected:
+    /** Store the verbosity level. */
     bool _quiet;
+
+    /** Store the image in cv::Mat format. */
     cv::Mat _img;
 
 public:
@@ -59,69 +62,68 @@ public:
 
     /**
      * Get the image in cv::Mat format.
-     * @param quiet suppress messages displayed upon success/failure.
      * @return the image, in cv::Mat format.
      */
     cv::Mat& getCvMat();
 
     /**
      * Binarize the image.
-     * @algorithm i.e. "redMinusGreen".
-     * @threshold i.e. 50.
+     * @param algorithm i.e. "redMinusGreen".
+     * @param threshold i.e. 50.
      */
     void binarize(const char* algorithm, const double threshold);
 };
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function gets the biggest contour.
  */
 vector <Point> getBiggestContour(const Mat image);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates X and Y.
  */
 void calcLocationXY(float& locX, float& locY, const vector <Point> biggestCont);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the mask.
  */
 void calcMask(Mat& mask, const vector <Point> biggestCont);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the area.
  */
 void calcArea(float& area, const vector <Point> biggestCont);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the rectangularity.
  */
 void calcRectangularity(float& rectangularity, const vector <Point> biggestCont);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the mass center.
  */
 void calcMassCenter(float& massCenterLocX, float& massCenterLocY , const vector <Point> biggestCont);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the aspect ratio.
  */
 void calcAspectRatio(float& aspectRatio, float& axisFirst, float& axisSecond ,const vector <Point> biggestCont);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the solidity.
  */
 void calcSolidity(float& solidity, const vector <Point> biggestCont);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the HSV mean and std deviation.
  */
 void calcHSVMeanStdDev(const Mat image, const Mat mask, float& hue_mean, float& hue_stddev,
@@ -129,29 +131,29 @@ void calcHSVMeanStdDev(const Mat image, const Mat mask, float& hue_mean, float& 
                        float& value_mean, float& value_stddev);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the HSV peak color.
  */
 void calcHSVPeakColor(const Mat image, const Mat mask, float& hue_mode, float& hue_peak,
                        float& value_mode, float& value_peak);
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the moments.
  */
 void calcMoments(Mat& theHuMoments, const vector <Point> biggestCont );
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the arc length.
  */
 void calcArcLength(float& arc, const vector <Point> biggestCont );
 
 /**
- * @ingroup TravisLib
+ * @ingroup travis_functions
  * This function calculates the circle.
  */
 void calcCircle(float& radius, const vector <Point> biggestCont );
 
-#endif  // __TRAVIS_LIB __
+#endif  // __TRAVIS_LIB_HPP__
 
