d11df21c1e81d06307b9edb43d54250c5127f46a
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-05-25 10:32:22 +0000

    clean and functional

--- main/TravisLib.hpp
+++ main/TravisLib.hpp
@@ -61,7 +61,7 @@ public:
     /**
      * This function calculates X and Y.
      */
-    void getBlobsXY(const vector <Point>& locations);
+    void getBlobsXY(vector <Point>& locations);
 
     /**
      * Get the image in cv::Mat format.
